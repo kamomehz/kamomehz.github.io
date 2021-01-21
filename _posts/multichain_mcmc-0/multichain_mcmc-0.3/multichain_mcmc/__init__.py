028b965fb18f5e2ba6cc8281d15840fb8ca6dcbe
Kamome <hezuo@KamomedeMacBook-Air.local>
2021-01-21 20:26:20 +0900

    update

--- _posts/multichain_mcmc-0/multichain_mcmc-0.3/multichain_mcmc/__init__.py
+++
@@ -1,4 +0,0 @@
-from dream import  DreamSampler
-from amala import  AmalaSampler
-from hmc import HamiltonianSampler
-import utilities
--- _posts/multichain_mcmc-0/multichain_mcmc-0.3/multichain_mcmc/examples/__init__.py
+++
@@ -1,1 +0,0 @@
-import amala_examples, dream_examples, hmc_exmaples
